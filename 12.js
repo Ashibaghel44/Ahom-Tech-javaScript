68302c238161bf9df3ee1b920c522bee9d55f8b1
Ashibaghel44 <138962968+Ashibaghel44@users.noreply.github.com>
2023-11-15 17:36:56 +0530

    first commit

--- 12.js
+++ 12.js
@@ -1,3 +1,11 @@
 // Create a function thatreceives an array of numbers as argument and returns an
 // array containing only the positive numbers
 
+
+let arr= [1,-2,-5,2,8,-9,3,-5,-6]
+let positive = extract(arr);
+
+function extract(inputArray){
+    return inputArray.filter(number=>number > 0) 
+}
+console.log(positive)
---
+++ 13.js
@@ -0,0 +1,6 @@
+// Find the maximum number in an array of numbers
+let arr =[12,27,36,45,58,76,53,43,34,12,34,76,95,100]
+for(let i =0 ; i<arr.length ; i++){
+    sum = Math.max(arr[i])
+}
+console.log(sum)
